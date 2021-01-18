0cbd9ba8732112d81bec822f03d7e6d02e8a65e1
thllwg <t_hell07@uni-muenster.de>
2021-01-18 16:02:48 +0100

    combine install instructions

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -3,6 +3,7 @@
 export ZSH=~/.oh-my-zsh
 
 if [ ! -d $ZSH ]; then
-  sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  sudo mv ~/.zshrc $HOME/.dotfiles/zsh/oh-my-zshrc
+  sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && \
+    sudo mv ~/.zshrc $HOME/.dotfiles/zsh/oh-my-zshrc && \
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions #install auto-suggestions
 fi
