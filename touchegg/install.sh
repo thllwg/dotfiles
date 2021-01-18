e52e2e34d52bc7088bcec7a4cf32ee156cedf073
thllwg <t_hell07@uni-muenster.de>
2021-01-18 13:38:20 +0100

    force lastversion to use .deb of touchegg

--- touchegg/install.sh
+++ touchegg/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pip3 install lastversion
-lastversion JoseExposito/touchegg --download 
+lastversion JoseExposito/touchegg --at github --assets --filter amd64.deb --download 
 sudo apt install -y ./touchegg_*.deb
 sudo rm ./touchegg_*.deb
