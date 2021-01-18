32e07b59a03db11b7b5321db39e157d4f899adf6
thllwg <t_hell07@uni-muenster.de>
2021-01-18 11:26:28 +0100

    hardwire dotfiles

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -4,5 +4,5 @@ export ZSH=~/.oh-my-zsh
 
 if [ ! -d $ZSH ]; then
   sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  sudo mv ~/.zshrc $DOTFILES/zsh/oh-my-zshrc
+  sudo mv ~/.zshrc $HOME/.dotfiles/zsh/oh-my-zshrc
 fi
